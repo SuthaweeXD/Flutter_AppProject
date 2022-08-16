1461596eb031a5f0fdfe549030be618feaaddb82
SuthaweeXD <suthawee.solaeh@gmail.com>
2022-08-16 21:26:31 +0700

    vs

---
+++ lib/views/order/CancleOrders.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_project/views/order/Orders.dart';
+
+class CancleOrders extends StatefulWidget {
+  CancleOrders({Key? key}) : super(key: key);
+
+  @override
+  State<CancleOrders> createState() => _CancleOrdersState();
+}
+
+class _CancleOrdersState extends State<CancleOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextButton(onPressed: , child: )
+        ],
+      ),
+    );
+  }
+}
